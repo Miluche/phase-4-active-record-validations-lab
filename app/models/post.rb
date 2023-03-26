1c590a1dbce8df986a3b956533bdecc552af8767
Miluche <meshkipla@gmail.com>
2023-03-26 10:46:57 +0300

    added all validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,15 @@
 class Post < ApplicationRecord
   validates :content, length: {minimum: 250}
   validates :summary, length: {maximum: 250}
-  validates :category, acceptance: { accept: %w[Fiction Non-Fiction] }
-  validates :title, presence: true, allow_blank: false
-  validate :same_title_for_all_posts , :is_click_bait
+  validates :category, inclusion: { in: %w(Fiction Non-Fiction) }
+  validates :title, presence: true
+  validate :title_is_click_bait
+  validate :same_title_for_all_posts
+
   private
-  def is_click_bait
+  def title_is_click_bait
     click_baits = ["Won't Believe", "Secret", "Top [number]", "Guess"]
-    if !click_baits.any? { |click_bait| title.include?(click_bait) } && !title.match(/Top \d+/)
+    if !click_baits.any? { |click_bait| title&.include?(click_bait) } && !title&.match(/Top \d+/)
       errors.add(:title, "The Post title does not qualify as click-bait")
     end
   end
